9743e84506ce790789994e62b0bbbf5dc1bd9bd0
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-03-16 10:10:22 +0700

    condition statement 2

---
+++ Module_1/Condition/ternary.js
@@ -0,0 +1,34 @@
+let name = "Châu";
+let gender = false;
+let married = true;
+
+// if(gender == true){
+//     console.log('Mr.' + name);
+// }
+// else{
+//     if(married == true){
+//         console.log('Mrs.' + name);
+//     }
+//     else{
+//         console.log('Ms.' + name);
+//     }
+// }
+
+// gender == true ? 
+//         console.log('Mr.' + name) : 
+//         (married == true ? 
+//             console.log('Mrs.' + name) : 
+//             console.log('Ms.' + name));
+
+// title. name
+console.log(`${gender == true ? "Mr" : (married == true ? "Mrs" : "Ms")}. ${name}`);
+
+// let a = 5;
+// let b = 8;
+// let c = 3;
+// // ax2 + bx + c = 0;
+// 5x2 + 8x + 3 = 0;
+
+// console.log(a + "x2 + " + b + "x + " + c + " = 0");
+
+// console.log(`${a}x2 + ${b}x + ${c} = 0`);
