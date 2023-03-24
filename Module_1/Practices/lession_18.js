f72255d2f216066e4698f00fc6e4f86cdcde13c4
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-03-24 10:55:27 +0700

    update

--- Module_1/Practices/lession_18.js
+++ Module_1/Practices/lession_18.js
@@ -1,26 +1,32 @@
-// let x = 2;
-// let number = 10;
-// let total = 0;
-// let totalFactorial = 1;
-// for (let n = 0; n <= number; n++) {
-//     totalFactorial = 1;
-//     for (let j = 1; j <= (2 * n); j++) {
-//         totalFactorial *= j;
-//     }
-//     total += x ** (2 * n) / totalFactorial;
-// }
-
-// console.log(total);
-
-let floats = new Array(10);
-let min = 10;
-let max = 80;
-for(let i =0; i < floats.length; i ++){
-    floats[i] = (Math.random()*(max - min + 1) + min).toFixed(2);
-    // floats[i] = Math.floor(Math.random()*(max - min + 1) + min);
+let x = 2;
+let number = 10;
+let total = 0;
+let totalFactorial = 1;
+for (let n = 0; n <= number; n++) {
+    total += x ** (2 * n) / calcFactorial(n);
 }
 
-console.log(floats);
+function calcFactorial(number){
+    totalFactorial = 1;
+    for (let j = 1; j <= (2 * number); j++) {
+        totalFactorial *= j;
+    }
+    return totalFactorial;
+}
+
+console.log(total);
+console.log(calcFactorial(10));
+console.log(calcFactorial(15));
+
+// let floats = new Array(10);
+// let min = 10;
+// let max = 80;
+// for(let i =0; i < floats.length; i ++){
+//     floats[i] = (Math.random()*(max - min + 1) + min).toFixed(2);
+//     // floats[i] = Math.floor(Math.random()*(max - min + 1) + min);
+// }
+
+// console.log(floats);
 
 // size
 // [min, max]
