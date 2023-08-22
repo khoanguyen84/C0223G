7c426fbb021ff2dd138205d355828788759fe223
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-08-22 10:24:34 +0700

    use navigate

--- Module_5/cra-app/src/App.js
+++ Module_5/cra-app/src/App.js
@@ -1,17 +1,20 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import Footer from './components/layout/Footer';
 import MainLayout from './components/layout/MainLayout';
 import Navabar from './components/layout/Navbar'
 import Main from './components/main/Main';
+import LoginPage from './components/Login/LoginPage';
 
 function App() {
   return (
     <>
-      {/* <Navabar/>
-      <Main/>
-      <Footer/> */}
       <MainLayout>
-        <Main />
+        <Routes>
+            <Route path='/' element={<LoginPage/>} />
+            <Route path='/login' element={<LoginPage/>} />
+            <Route path='/main' element={<Main/>} />
+        </Routes>
       </MainLayout>
     </>
   );
